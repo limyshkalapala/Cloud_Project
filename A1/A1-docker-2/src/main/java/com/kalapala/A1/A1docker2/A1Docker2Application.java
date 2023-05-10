4956acc21480bee9986c30ad2aac9de1b461f217
limyshkalapala <limysh@dal.ca>
2023-05-10 11:39:10 -0300

    A1 project structure created according to requirement.

--- A1/A1-docker-2/src/main/java/com/kalapala/A1/A1docker2/A1Docker2Application.java
+++ A1/A1-docker-2/src/main/java/com/kalapala/A1/A1docker2/A1Docker2Application.java
@@ -1,13 +1,13 @@
-package com.kalapala.A1;
+package com.kalapala.A1.A1docker2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class A1Application {
+public class A1Docker2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(A1Application.class, args);
+		SpringApplication.run(A1Docker2Application.class, args);
 	}
 
 }
--- A1/A1-docker-2/src/test/java/com/kalapala/A1/A1docker2/A1Docker2ApplicationTests.java
+++ A1/A1-docker-2/src/test/java/com/kalapala/A1/A1docker2/A1Docker2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kalapala.A1;
+package com.kalapala.A1.A1docker2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class A1ApplicationTests {
+class A1Docker2ApplicationTests {
 
 	@Test
 	void contextLoads() {
