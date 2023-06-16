7cdfef2809758ca8755a7d4a9b0c656e30fe8982
limyshkalapala <limysh@dal.ca>
2023-06-16 16:08:36 -0300

    " fix store file"

--- src/main/java/com/kalapala/A1/A1docker1/ListenerController.java
+++ src/main/java/com/kalapala/A1/A1docker1/ListenerController.java
@@ -1,5 +1,6 @@
 package com.kalapala.A1.A1docker1;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,28 +64,99 @@ public class ListenerController {
         }
     }
 
+
+
+
+
+
+
+
+
     @PostMapping(value = "/store-file")
-    public ResponseEntity<String> storeFile(@RequestBody FileRequestDTO fileRequestDTO) throws IOException {
+    public ResponseEntity<String> storeFile(@RequestBody FileRequestDTO fileRequestDTO) {
         if (fileRequestDTO.getFile() == null) {
-            ErrorResponse errorResponse = new ErrorResponse(fileRequestDTO.getFile(), "Invalid JSON input.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(convertObjectToJsonString(errorResponse));
+            ErrorResponseStoreFile errorResponseStoreFile = new ErrorResponseStoreFile(null, "Invalid JSON input.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(convertObjectToJsonStringStoreFile(errorResponseStoreFile));
         }
 
         String filePath = "/app/" + fileRequestDTO.getFile();
         File file = new File(filePath);
 
-        // Write the file to the persistent volume
         try {
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(fileRequestDTO.getData());
             fileWriter.close();
         } catch (IOException e) {
-            ErrorResponse errorResponse = new ErrorResponse(fileRequestDTO.getFile(), "Error storing the file.");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(convertObjectToJsonString(errorResponse));
+            ErrorResponseStoreFile errorResponseStoreFile = new ErrorResponseStoreFile(fileRequestDTO.getFile(), "Error while storing the file to the storage.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(convertObjectToJsonStringStoreFile(errorResponseStoreFile));
         }
 
-        return ResponseEntity.ok("File stored successfully.");
+        SuccessResponseStoreFile successResponseStoreFile = new SuccessResponseStoreFile(fileRequestDTO.getFile(), "Success.");
+        return ResponseEntity.ok(convertObjectToJsonStringStoreFile(successResponseStoreFile));
     }
+
+    private String convertObjectToJsonStringStoreFile(Object object) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            // Handle the exception or return a default error response
+            return "{\"error\": \"Failed to convert object to JSON.\"}";
+        }
+    }
+
+    class ErrorResponseStoreFile {
+        private String file;
+        private String error;
+
+        public ErrorResponseStoreFile(String file, String error) {
+            this.file = file;
+            this.error = error;
+        }
+
+        public String getFile() {
+            return file;
+        }
+
+        public void setFile(String file) {
+            this.file = file;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
+    }
+
+    class SuccessResponseStoreFile {
+        private String file;
+        private String message;
+
+        public SuccessResponseStoreFile(String file, String message) {
+            this.file = file;
+            this.message = message;
+        }
+
+        public String getFile() {
+            return file;
+        }
+
+        public void setFile(String file) {
+            this.file = file;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
 
 
 
